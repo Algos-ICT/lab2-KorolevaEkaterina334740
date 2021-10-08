51e69cced6d39b9d360640c1c8ad0cf87871a1a1
KorolevaEkaterina334740 <90193181+KorolevaEkaterina334740@users.noreply.github.com>
2021-10-08 15:48:19 +0300

    Add files via upload

---
+++ алгоритмы 2.1.py
@@ -0,0 +1,21 @@
+import time
+t_start = time.perf_counter()
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        j = i
+        while j > 0 and arr[j] < arr[j-1]:
+            arr[j], arr[j-1] = arr[j-1], arr[j]
+            j -= 1
+
+
+with open('input.txt') as f:
+    n = int(f.readline())
+    a = f.readline()
+a = a.split()
+for i in range(n):
+    a[i] = int(a[i])
+insertion_sort(a)
+with open('output.txt', 'w') as f:
+    f.write(str(a))
+print(a)
+print("Время работы: %s секунд" % (time.perf_counter() - t_start))
